ee9750dabbc43269c6600659f982ab191699d941
Jin Shengxi <jinshengxi777@gmail.com>
2022-10-18 19:17:30 -0400

    quick REST api test

---
+++ src/main/java/com/joeyjin/rewardsProgram/controller/RewardsRestController.java
@@ -0,0 +1,16 @@
+package com.joeyjin.rewardsProgram.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+public class RewardsRestController {
+
+    @GetMapping(value = "/")
+    public ResponseEntity<String> getHomepage()  {
+        String homepageMsg = "This is the Customer Rewards program API root.";
+        return new ResponseEntity<>(homepageMsg, HttpStatus.OK);
+    }
+}
